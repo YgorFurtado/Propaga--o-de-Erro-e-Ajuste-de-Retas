fea5ab5e110b635089a74377918450e4e285b43c
Ygor Furtado <ygorfurt@gmail.com>
2024-02-28 16:09:43 -0300

    Correção erro

--- Ajuste MMQ.py
+++ Ajuste MMQ.py
@@ -1,17 +1,18 @@
 from colorama import Fore, Back, Style
 
 n = int(input("Quantidade de Dados:"))
+# Em y temos a pressão e seu erro, enquanto no x temos 1/h
 Eixo_X = []
 Erro_X = []
 Eixo_Y = []
 Erro_Y = []
 
 for i in range(0,n):
-    Eixo_X.append(float(input(f"Inserir x{i+1}: ")))
-for i in range(0,n):
-    Erro_X.append(float(input(f"Inserir σx{i+1}: ")))
-for i in range(0,n):
     Eixo_Y.append(float(input(f"Inserir y{i+1}: ")))
+for i in range(0,n):
+    Erro_Y.append(float(input(f"Inserir σy{i+1}: ")))
+for i in range(0,n):
+    Eixo_X.append(float(input(f"Inserir x{i+1}: ")))
 #for i in range(0,n):
 #    Erro_Y.append(float(input(f"Inserir σy{i+1}: ")))
 
@@ -19,30 +20,30 @@ for i in range(0,n):
 #Calcula <σ2>
 Sig = 0
 for i in range(0,n):
-    Sig = Sig + (1/(Erro_X[i]**2))
-    
+    Sig = Sig + (1/(Erro_Y[i]**2))
+
 #Calcula <x>
 x = 0
 for i in range(0,n):
-    x = x + (Eixo_X[i]/Erro_X[i])
+    x = x + (Eixo_X[i]/Erro_Y[i])
 x = (1/Sig)*x
 
 #Calcula <x²>
 xsqr = 0
 for i in range(0,n):
-    xsqr = xsqr + ((Eixo_X[i]**2)/Erro_X[i])
+    xsqr = xsqr + ((Eixo_X[i]**2)/Erro_Y[i])
 xsqr = (1/Sig)*xsqr
 
 #Calcula <y>
 y = 0
 for i in range(0,n):
-    y = y + (Eixo_Y[i]/Erro_X[i])
+    y = y + (Eixo_Y[i]/Erro_Y[i])
 y = (1/Sig)*y
 
 #Calcula <xy>
 xy = 0
 for i in range(0,n):
-    xy = xy + ((Eixo_X[i]*Eixo_Y[i])/Erro_X[i])
+    xy = xy + ((Eixo_X[i]*Eixo_Y[i])/Erro_Y[i])
 xy = (1/Sig)*xy
 
 #Calcula a,b
